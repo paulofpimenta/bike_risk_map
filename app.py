0875dcbecb5184765d8cd3e7ff7d98fbb844eceb
Paulo Pimenta <pfpimenta@gmail.com>
2024-01-11 03:22:55 +0100

    More improvements on dashboard

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import plotly.express as px
 import pandas as pd
 import geopandas as gpd
 import dash_bootstrap_components as dbc
+import dash
+from itertools import product 
+import datetime
 
 external_stylesheets = [dbc.themes.BOOTSTRAP]
 
@@ -16,21 +19,25 @@ server = app.server
 
 accidents_data = pd.read_csv("./data/accidents_by_zone.csv", index_col=[0])
 accidents_data['index'] = accidents_data.index
-print(accidents_data)
+
 arrondis_gdf = gpd.read_file("./data/arrondissements.geojson")
 
 zones_gdf = gpd.read_file("./data/zones.geojson")
-paris_accidents_ll = gpd.read_file("./data/paris_accidents.geojson")
-month_list = paris_accidents_ll.an.unique()
+
+accidents_paris_ll = gpd.read_file("./data/accidents_paris_ll.geojson")
+lats = accidents_paris_ll.get_coordinates().y.to_list()
+lons = accidents_paris_ll.get_coordinates().x.to_list()
+
+months_order = ['janvier', 'février', 'mars', 'avril',
+          'mai', 'juin', 'juillet', 'août',
+          'septembre', 'octobre', 'novembre', 'décembre']
+
 # zones_df = pd.read_csv()
 # 48.86260697107489, 2.346794478444374
 with open("./data/zones.json") as response:
     zones = json.load(response)
 
-print(month_list)
 # APP Layout
-#'' 'mai' '' '' 'janvier' 'mars' '' 'juin'
-# 'août' '' '' 'décembre'
 
 SIDEBAR_STYLE = {
     "left": 0,
@@ -40,44 +47,62 @@ SIDEBAR_STYLE = {
 }
 
 
+
+available_years = [years for years in accidents_paris_ll['an'].unique()]
+
+year_month_dict = {}
+
+for year in available_years:
+    months = accidents_paris_ll[accidents_paris_ll['an'] == year]['mois'].unique().tolist()
+    year_month_dict[year] = [months for _, months in sorted(zip(months_order, months))]
+
+names = list(year_month_dict.keys())
+nestedOptions = year_month_dict[names[0]]
+
 controls = dbc.Card(
     [
         html.Div(
             [
-                dbc.Label("X variable"),
+                dbc.Label("Year"),
                 dcc.Dropdown(
-                    id="selected_month",
-                    options=[
-                        {"label": "January", "value": "janvier"},
-                        {"label": "February", "value": "février"},
-                        {"label": "Mars", "value": "mars"},
-                        {"label": "April", "value": "avril"},
-                        {"label": "May", "value": "mai"},
-                        {"label": "June", "value": "juin"},
-                        {"label": "July", "value": "juillet"},
-                        {"label": "August", "value": "août"},
-                        {"label": "September", "value": "septembre"},
-                        {"label": "October", "value": "octobre"},
-                        {"label": "November", "value": "novembre"},
-                        {"label": "December", "value": "décembre"},
-                    ],
+                    id='year_dropdown',
+                    options=[{'label':name, 'value':name} for name in names],
                     multi=False,
-                    value="janvier",
-                    style={"width": "40%"}
+                    clearable=False,
+                    value = list(year_month_dict.keys())[0],
+                    style={"width": "40%","height": "100%"}
                 ),
-            ],
+                dbc.Label("Month"),
+                dcc.Dropdown(
+                    id='month_dropdown',
+                    clearable=False,
+                    style={"width": "40%","height": "100%"}
+                ),
+            ], style={'top': '0'}
         ),
+        html.Br(),
         html.Div(
             [
-                dbc.Label("Y variable"),
+                dbc.Label("Gravity"),
                 dcc.RadioItems(
-                    id='year', 
-                    options=["Joly", "Coderre", "Bergeron"],
-                    value="Coderre",
+                    id='radio_gravity', 
+                    options=["1", "2", "3","4"],
+                    value="2",
                     inline=True
                 ),
+                dbc.Label("Show accident's locations ?"),
+                dcc.Checklist(
+                    id="check_show_accidents",
+                    options={
+                        'Yes': True,
+                        'No': False
+                        },
+                    value=['No'],
+                    inline=True
+                )
             ]
         ),
+        html.Br(),
         html.Div(
             [
                 dbc.Label("Cluster count"),
@@ -86,81 +111,113 @@ controls = dbc.Card(
         ),
     ],
     body=True,
+    style={'height': "100%"}
 )
+
 
 app.layout = dbc.Container(
     [
-        html.H1("Bicycle accidents in Paris"),
+        html.H1("Bicycle accidents in Paris", style={'textAlign': 'center'}),
         html.Hr(),
         dbc.Row(
             [
                 dbc.Col(controls, md=5),
-                dbc.Col(dcc.Graph(id="graph2"), md=7),
+                dbc.Col(dcc.Graph(id="map_graph"), md=7),
             ],
-            align="center",
+            align="top",
         ),
         dbc.Row(
             [
-                dbc.Col(dcc.Graph(id="graph1"), md=6),
+                dbc.Col(dcc.Graph(id="month_graph"), md=6),
+                dbc.Col(dcc.Graph(id="hours_graph"), md=6),
 
             ]
         ),
     ],
     fluid=True,
 )
-    
+
+@app.callback(
+    dash.dependencies.Output('month_dropdown', 'options'),
+    [dash.dependencies.Input('year_dropdown', 'value')]
+)
+def update_date_dropdown(name):
+    months_dict = {'janvier': 0, 'février': 1, 'mars': 2, 'avril': 3, 'mai': 4, 'juin': 5, 'juillet': 6, 
+    'août': 7, 'septembre': 8, 'octobre': 9, 'novembre': 10, 'décembre': 11}
+    ordered_month_list = sorted(year_month_dict[name], key=lambda x: months_dict[x.lower()])
+
+
+    return [{'label': i, 'value': i} for i in ordered_month_list]
+
+
 
 # Callback functions
-@app.callback(Output("graph1", "figure"), [Input("selected_month", "value")])
-def cb(selected_month):
-    month = selected_month if selected_month else "janvier"
-    
+@app.callback(
+    [Output(component_id="month_graph", component_property="figure"),
+     Output(component_id="hours_graph", component_property="figure")],
+    [Input("month_dropdown", "value"),
+     Input("year_dropdown", "value")])
+def months_and_hours_graph(month,year):
+    selected_month = month if month else 'janvier'
+    selected_year = year if year else 2011
+    print("Selected year and month : " ,selected_year,selected_month)
+    # Filter year
+    accidents_paris_ll_by_year = accidents_paris_ll.loc[(accidents_paris_ll['an'] == int(selected_year))]
+    # Filter month
+    accidents_paris_ll_by_year_month = accidents_paris_ll.loc[(accidents_paris_ll['an'] == int(selected_year)) & (accidents_paris_ll['mois'] == selected_month)]
     # Group and sort accidents by month
-    #accidents_paris_grouped = accidents_paris.sort_values('mois', ascending=True).groupby('mois').size().reset_index(name ='Accidents')
-    #accidents_paris_sorted_grouped = accidents_paris_grouped.sort_values('mois', key=lambda s: s.apply(months_order.index), ignore_index=True)
-
+    accidents_paris_sorted_month = accidents_paris_ll_by_year.sort_values('mois', ascending=True).groupby('mois').size().reset_index(name ='Accidents')
+    accidents_paris_sorted_month_grouped = accidents_paris_sorted_month.sort_values('mois', key=lambda s: s.apply(months_order.index), ignore_index=True)
     # Group and sort accidents by hour
-    #accidents_paris_grouped_hour = accidents_paris.sort_values('hour', ascending=True).groupby('hour').size().reset_index(name ='Accidents')
+    accidents_paris_grouped_hour = accidents_paris_ll_by_year_month.sort_values('hour', ascending=True).groupby('hour').size().reset_index(name ='Accidents')
     
-    points_month_selected = paris_accidents_ll[paris_accidents_ll["mois"] == month]
-    points_years_of_selected_months = points_month_selected[['an']]
-    grouped_by_year = points_years_of_selected_months.groupby(["an"]).agg({'an': "sum"}).rename(columns={"an": "total"})
-    print(grouped_by_year)
-    #points_accidents_sum_month = points_month_selected[paris_accidents_ll["mois"] == month]
+    months_graph = px.line(accidents_paris_sorted_month_grouped, x='mois', y='Accidents', )
+    months_graph.update_layout(
+        title={
+            'text': "Accidents in <b>" + str(selected_year) + "</b>",
+            'y':0.94,
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'})
+    hours_graph = px.line(accidents_paris_grouped_hour, x='hour', y='Accidents')
+    hours_graph.update_layout(
+        title={
+            'text': "Accidents in <b>" + selected_month + "</b> per day hour",
+            'y':0.94,
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'})
 
-    return px.line(grouped_by_year, title='Accidents per year')
+    return months_graph, hours_graph
 
 
 @app.callback(
-     Output("graph2", "figure"),
-     [Input("selected_month", "value")],
-     #[Input("graph", "selectedData")],
+     Output("map_graph", "figure"),
+     [Input("month_dropdown", "value")],
  )
 def display_selected_data(points_month):
+    color_continuous_scale=["green", "yellow", "orange","red"]
     #month = selected_month if selected_month else "janvier"
     #points_month = zones_gdf.query("month_list == @month_list")
     #if selectedData:
     #    indices = [point["customdata"][0] for point in selectedData["points"]]
     #    points_month = points_month.loc[indices]
-    print(accidents_data)
     fig = px.choropleth_mapbox(accidents_data, geojson=zones_gdf,
                         locations="index",
                         labels={"num_acc_by_area": "number of accidents"},
-                        color_continuous_scale="Viridis",
+                        color_continuous_scale=color_continuous_scale,
                         color='num_acc_by_area',
-                        mapbox_style="carto-positron",
                         zoom=11, center = {"lat": 48.85848828830715, "lon": 2.351379571148244},
                         template='seaborn',
+                        mapbox_style="open-street-map",
                         opacity=0.6,
                         hover_name="index", 
-                        hover_data={'index':False})
-                        
+                        hover_data={'index':False})            
     fig.update_geos(fitbounds="locations", visible=False)
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-    fig.update_layout(legend_xref="paper")
-    fig.update_layout(mapbox_style="open-street-map")
-    fig.update_coloraxes(cauto=False)
-    fig.update_coloraxes(colorbar_ypad=0)
+
+    fig.add_scattermapbox(lat=lats, lon=lons, marker_size=6, marker_color='rgb(0, 0, 0)',opacity=0.3,hoverinfo = "skip")
+    
     return fig
 
 
