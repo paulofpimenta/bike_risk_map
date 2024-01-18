172dedb6c41510ddebdb784d34325f26285f6a03
Paulo Pimenta <pfpimenta@gmail.com>
2024-01-18 12:54:22 +0100

    Changed docker file

--- application/app.py
+++ application/app.py
@@ -248,5 +248,5 @@ def create_app():
 
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(flask_app, host="0.0.0.0", port=80)
+    #from waitress import serve
+    #serve(flask_app, host="0.0.0.0", port=80)
